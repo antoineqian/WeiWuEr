d02b4d7b97bdefac76c7f96842716495bc72ae5f
Antoine Qian <antoine.qian@protonmail.com>
2025-06-06 11:11:29 +0200

    Default particle system

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import ParticleSystem from './ParticleSystem'
 
 function App() {
   return (
     <Canvas style={{ height: '100vh', background: 'black' }}>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="hotpink" />
-      </mesh>
+      <ParticleSystem count={8000} />
       <OrbitControls />
     </Canvas>
-  );
+  )
 }
 
-export default App;
+export default App
